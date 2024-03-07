a9c42b7085e52d21e12f5373f2fa31c2ed3cdbc7
ckyu <dbcjdrb1997@gmail.com>
2024-03-07 13:36:00 +0900

    [Camera-Object-Detect] Mouse , Cat, Person ...

---
+++ app/src/main/java/com/bluesky/basic_tensorflow/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.bluesky.basic_tensorflow
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bluesky.basic_tensorflow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+    }
+
+    override fun onBackPressed() {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+            // Workaround for Android Q memory leak issue in IRequestFinishCallback$Stub.
+            // (https://issuetracker.google.com/issues/139738913)
+            finishAfterTransition()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
